4eccf2a78e65131ff1e46b88d83fe70158bdc78e
2638829795 <89207817+2638829795@users.noreply.github.com>
2021-08-20 11:01:20 +0800

    bbq
    bbq

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php require_once( './header.php'); ?>
+<div class="mdui-card">
+    <div class="mdui-card-header">
+        <img class="mdui-card-header-avatar" src="./assets/img/nssy.png" />
+        <div class="mdui-card-header-title">绵阳南山中学双语学校表白墙</div>
+        <div class="mdui-card-header-subtitle">一个简洁美观的校内表白墙</div>
+    </div>
+    <div class="mdui-card-media">
+        <img src="http://soxft.cn/assets/img/team.png" />
+    </div>
+    <div class="mdui-card-content">版本：2.2
+        <br />
+        <br />网站搭建：亦云
+        <br />
+        <br />源码作者：XCSOFT
+        <br />
+        <br />使用语言(框架)：PHP HTML MDUI MYSQL
+        <br />
+        <br />网页icon来自阿里巴巴矢量图标库由KERRY_ZJX发布,如有侵权请予以联系进行删除
+    </div>
+    <div class="mdui-card-actions">
+<a class="mdui-btn mdui-ripple" href="//www.yi.confession.red">亦云小站</a>
+<a class="mdui-btn mdui-ripple" href="//blog.xsot.cn/archives/pro-bbq.html">源码作者官网</a>
+<a class="mdui-btn mdui-ripple" href="//github.com/soxft/Lovewall">源码Github</a>
+    </div>
+</div>
+<br />
+<?php require_once( './footer.php'); ?>
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$conn=mysqli_connect("localhost","xcbbq","040216","xcbbq");
+$conns=mysqli_connect("localhost","xcbbq","040216","information_schema");
+//你的数据库信息
+$px=25              
+//每页显示表白数
+?>
